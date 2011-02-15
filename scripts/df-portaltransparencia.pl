d58971778190680fe6c06798c1ce8648143cab27
Alexei Znamensky <russoz@cpan.org>
2011-02-15 21:28:36 -0200

    df-portaltransparencia.pl: multiple changes
    * removed 'use' not being used
    * removed initial_data param from chain
    * added LiteralData to chain
    * added a separate NOP node to de-reference
    * ran through perltidy
    
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- scripts/df-portaltransparencia.pl
+++ scripts/df-portaltransparencia.pl
@@ -9,23 +9,26 @@ use lib "$Bin/../lib";
 use aliased 'DataFlow::Node';
 use aliased 'DataFlow::Chain';
 use aliased 'DataFlow::Node::LiteralData';
+use aliased 'DataFlow::Node::NOP';
 use aliased 'DataFlow::Node::HTMLFilter';
 use aliased 'DataFlow::Node::URLRetriever';
 use aliased 'DataFlow::Node::MultiPageURLGenerator';
+use aliased 'DataFlow::Node::Dumper' => 'DumperNode';
 use aliased 'DataFlow::Node::SQL';
-
-#use Scalar::Util qw/reftype/;
 
 my $base = join( '/',
     q{http://www.portaltransparencia.gov.br},
     q{ceis}, q{EmpresasSancionadas.asp?paramEmpresa=0} );
 
 my $chain = Chain->new(
-    initial_data  => [ $base ],
     links => [
+        LiteralData->new($base),
+
         #DumperNode->new,
         MultiPageURLGenerator->new(
+            name       => 'multipage',
             first_page => -2,
+
             #last_page     => 35,
             produce_last_page => sub {
                 my $url = shift;
@@ -41,7 +44,8 @@ my $chain = Chain->new(
                 my $texto =
                   HTML::TreeBuilder::XPath->new_from_content($html)
                   ->findvalue('//p[@class="paginaAtual"]');
-                die q{Não conseguiu determinar a última página} unless $texto;
+                die q{Não conseguiu determinar a última página}
+                  unless $texto;
                 return $1 if $texto =~ /\d\/(\d+)/;
             },
             make_page_url => sub {
@@ -54,34 +58,47 @@ my $chain = Chain->new(
                 return $u->as_string;
             },
         ),
-        #DumperNode->new,
+        NOP->new( deref => 1, name => 'nop' ),
+
+		#Node->new( name => 'snoopy', process_item => sub { no strict; use Data::Dumper; print STDERR 'snoop: '.Dumper(eval '$chain')."\n" } ),
+		#DumperNode->new,
         URLRetriever->new( process_into => 1, ),
+
         #DumperNode->new,
         HTMLFilter->new(
+            process_into => 1,
             search_xpath =>
               '//div[@id="listagemEmpresasSancionadas"]/table/tbody/tr',
         ),
+
         #DumperNode->new,
         HTMLFilter->new(
             search_xpath => '//td',
             result_type  => 'VALUE',
             ref_result   => 1,
         ),
-#        Node->new(
-#            process_into => 1,
-#            process_item => sub {
-#                shift; print STDERR 'type = ', shift, "\n";
-#            },
-#        ),
+
+        #DumperNode->new,
+
+        #Node->new(
+        #    process_into => 1,
+        #    process_item => sub {
+        #        shift; print STDERR 'type = ', shift, "\n";
+        #    },
+        #),
         Node->new(
             process_into => 1,
             process_item => sub {
-                shift; local $_ = shift;
-                s/^\s*//; s/\s*$//;
+                shift;
+                local $_ = shift;
+                s/^\s*//;
+                s/\s*$//;
                 return $_;
             }
         ),
-        SQL->new( table => 'ceis' ),
+
+        #SQL->new( table => 'ceis' ),
+        DumperNode->new,
     ],
 );
 
