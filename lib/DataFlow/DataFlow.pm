b613f0028a166d48db10fe9d855a183072cb8961
Alexei Znamensky <russoz@cpan.org>
2011-02-23 22:53:02 -0300

    added a DataFlow.pm, mostly for documentation
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

---
+++ lib/DataFlow/DataFlow.pm
@@ -0,0 +1,46 @@
+package DataFlow;
+
+use strict;
+use warnings;
+
+# VERSION
+
+1;
+
+=pod
+
+=head1 NAME
+
+DataFlow - A framework for dataflow processing
+
+=head1 SYNOPSIS
+
+	use DataFlow::Node;
+	use DataFlow::Chain;
+
+	my $chain = DataFlow::Chain->new(
+		DataFlow::Node->new(
+			process_item => sub {
+				... do something
+			}
+		),
+		DataFlow::Node->new(
+			process_item => sub {
+				... do something else
+			}
+		),
+	);
+
+	my $output = $chain->process($input);
+
+=head1 DESCRIPTION
+
+This is a framework for data flow processing. It started as a spinoff project
+from L<OpenData-BR|http://www.opendatabr.org/>.
+
+As of now (Feb, 2011) it is still a 'work in progress', and there is a lot of
+progress to make. It is highly recommended that you read the tests, and also
+the documentation for L<DataFlow::Node> and L<DataFlow::Chain>, to start with.
+
+=cut
+
