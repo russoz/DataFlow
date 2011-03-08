944c153a2fb732791b8bb101f2df10bc6779d7ac
Alexei Znamensky <russoz@cpan.org>
2011-03-08 18:10:51 -0300

    improved CSV node
    now it can convert from CSV and to CSV
    
    Signed-off-by: Alexei Znamensky <russoz@cpan.org>

--- lib/DataFlow/Node/CSV.pm
+++ lib/DataFlow/Node/CSV.pm
@@ -1,67 +1,87 @@
 package DataFlow::Node::CSV;
 
-#ABSTRACT: A CSV converting node
-
 use strict;
 use warnings;
 
+# ABSTRACT: A CSV converting node
 # VERSION
 
 use Moose;
 extends 'DataFlow::Node';
 
-use Carp;
+use Moose::Util::TypeConstraints;
 use Text::CSV;
 
-has 'header' => (
+has 'headers' => (
     'is'        => 'ro',
     'isa'       => 'ArrayRef[Str]',
-    'predicate' => 'has_header',
+    'predicate' => 'have_headers',
 );
 
-has 'inject_header' => (
+has '_header_unused' => (
     'is'      => 'ro',
     'isa'     => 'Bool',
-    'default' => 0,
+    'default' => 1,
+    'clearer' => '_use_header',
+);
+
+enum '_direction_type' => [qw/FROM_CSV TO_CSV/];
+
+has 'direction' => (
+    'is'       => 'ro',
+    'isa'      => '_direction_type',
+    'required' => 1,
 );
 
 has 'text_csv_opts' => (
-    'is'        => 'ro',
-    'isa'       => 'HashRef',
-    'predicate' => 'has_text_csv_opts',
+    'is'  => 'ro',
+    'isa' => 'HashRef',
 );
 
 has 'csv' => (
     'is'      => 'ro',
     'isa'     => 'Text::CSV',
-    'lazy'    => 1,
     'default' => sub {
         my $self = shift;
 
         my $make_csv = sub {
-            return Text::CSV->new(
-                $self->has_text_csv_opts ? $self->text_csv_opts : undef );
+            return Text::CSV->new( $self->text_csv_opts );
         };
-        return $make_csv->() unless $self->inject_header;
+        return $make_csv->() unless $self->have_headers;
 
-        croak 'Thou hast requested to inject headers but,'
-          . 'alas, no header has been provided'
-          unless ( $self->has_header );
+        # use headers
+        if ( $self->direction eq 'TO_CSV' ) {
+            $self->input( $self->headers );
+        }
 
-        $self->_add_output( $self->deref ? @{ $self->header } : $self->header );
         return $make_csv->();
     },
 );
 
-has '+process_item' => (
-    'default' => sub {
-        return sub {
-            my ( $self, $data ) = @_;
-            return $data unless ref($data) eq 'ARRAY';
+before 'process_input' => sub {
+    my $self = shift;
+    return unless $self->direction eq 'TO_CSV';
+    $self->add_output( $self->headers );
+    $self->_use_header;
+};
 
-            $self->csv->combine( @{$data} );
-            return $self->csv->string;
-          }
+sub _to_csv {
+    my ( $self, $data ) = @_;
+    $self->csv->combine( @{$data} );
+    return $self->csv->string;
+}
+
+sub _from_csv {
+    my ( $self, $csvline ) = @_;
+    $self->csv->parse($csvline);
+    return [ $self->csv->fields ];
+}
+
+has '+process_item' => (
+    'lazy'    => 1,
+    'default' => sub {
+        return \&_to_csv if shift->direction eq 'TO_CSV';
+        return \&_from_csv;
     }
 );
 
